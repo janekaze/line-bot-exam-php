7a5b3c432f39c486cab3b1ad6cf2d05511c9f0de
janekaze <35206669+janekaze@users.noreply.github.com>
2018-07-02 11:42:40 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -10,7 +10,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 //Reply MSG
-replyMessage($access_token)
+replyMessage("OK")
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
