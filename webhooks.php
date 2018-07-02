0919494dcf208b69537ce3bae9f193fa82c6b436
janekaze <35206669+janekaze@users.noreply.github.com>
2018-07-02 16:20:23 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -5,6 +5,7 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 $access_token = 'AklGxwOyremF+3ylRbytkKDzWvNBp6RHjvZ6nf+itkgDU4RFLdoPeUULnip1HrmEVtFom7cp0zpfixl4Y4S7zBOnpPCadx7/mIasRuQqZGFd242oxeI8YUr3z9PbouCKygMyDXGKfyUrY5+GAL3F/AdB04t89/1O/w1cDnyilFU=';
 $User_Token = '794GVAyhWqChqCwXnUTVgXrmWOtlWVeAKEj6msS6J7F';
+$channelSecret = '1ca3d4f6259fa05c8103c916eac88eb6';
 $User_ID = 'U0954bd96fe543817013f1d5ae9998a54';
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -53,34 +54,10 @@ if (!is_null($events['events'])) {
 		}
 		if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
 			$MSG_ID = 'MSG ID: ' . $event['message']['id'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $MSG_ID
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://notify-api.line.me/api/notify';
-			$data = [
-				'to' => $User_ID,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $User_Token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";			
+			$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+			$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($MSG_ID);
+			$response = $bot->pushMessage($User_ID, $textMessageBuilder);		
 		}
 	}
 }
